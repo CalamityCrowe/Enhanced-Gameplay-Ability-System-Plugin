fd6ca4b3222849e595a65d917042cef0e278dffd
Kieran T <kieran123416@gmail.com>
2026-08-01 18:30:54 +0100

    Starting weapon setup
    starting the weapon setup for the characters to use in the system. not sure how I am going to handle the fire logic so will need to think about this

---
+++ Plugins/EnhancedGameplayAbilitySystem/Source/EnhancedGameplayAbilitySystem/Public/Weapons/WeaponBase.h
@@ -0,0 +1,38 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WeaponBase.generated.h"
+
+class UPooledProjectilesSubsystem;
+
+UCLASS()
+class ENHANCEDGAMEPLAYABILITYSYSTEM_API AWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeaponBase();
+	
+	
+protected:
+	UFUNCTION()
+	void OnWeaponOwnerDestroyed(AActor* DestroyedActor); 
+	
+	virtual void BeginPlay() override;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USkeletalMeshComponent> WeaponMesh; 
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> HitStart;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = true))
+	TObjectPtr<USceneComponent> HitEnd;
+	
+	UPROPERTY()
+	UPooledProjectilesSubsystem* ProjectilePoolSubsystem; 
+};
